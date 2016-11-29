310c8f94a8596bddec773c5a154165e7b90fb98e
oldgizahub <geoffgigg@hotmail.com>
2016-11-29 16:54:03 -0500

    new French translations for mx-remastercc

--- translations/mx-remastercc_fr.ts
+++ translations/mx-remastercc_fr.ts
@@ -1,6 +1,4 @@
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS>
-<TS version="2.1" language="fr">
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="fr" version="2.1">
 <context>
     <name>mxremastercc</name>
     <message>
@@ -11,12 +9,12 @@
     <message>
         <location filename="../mxremastercc.ui" line="65"/>
         <source>Persistence</source>
-        <translation>Persistence</translation>
+        <translation>Persistance</translation>
     </message>
     <message>
         <location filename="../mxremastercc.ui" line="80"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;This tool allows you to rework an ISO while running Live. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remaster&lt;/span&gt; allows you to change files, applications and settings with the goal of producing a new master copy for storage or distribution. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistence&lt;/span&gt; allows you to retain data, settings and installed programs when the machine gets switched off for personal use.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Cet outil vous permet de modifier un ISO en utilisant un USB live&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remaster&lt;/span&gt;vous permet de changer les fichiers, applications et paramètres avec l&apos;objectif de produire une nouvelle copie de maître pour le stockage ou la distribution.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistence.&lt;/span&gt;vous permet de conserver les données, paramètres et programmes installés lorsque la machine s&apos;éteint pour un usage personnel.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Cet outil vous permet de modifier un ISO lors d&apos;une session Live&lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remastériser&lt;/span&gt; vous permet de changer les fichiers, applications et paramètres dans le but de produire un nouvel ISO original en vue de son stockage ou de sa distribution.&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistance&lt;/span&gt; vous permet de conserver, après extinction de la machine, les données, paramètres et programmes installés pour votre usage personnel.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../mxremastercc.ui" line="132"/>
@@ -71,12 +69,12 @@
     <message>
         <location filename="../mxremastercc.ui" line="267"/>
         <source>Remaster</source>
-        <translation>Remaster</translation>
+        <translation>Remastériser</translation>
     </message>
     <message>
         <location filename="../mxremastercc.ui" line="292"/>
         <source>Save root persistence</source>
-        <translation>Enregistrer la persistance racine</translation>
+        <translation>Enregistrer la persistance &quot;racine&quot;</translation>
     </message>
     <message>
         <location filename="../mxremastercc.ui" line="304"/>
@@ -86,7 +84,7 @@
     <message>
         <location filename="../mxremastercc.ui" line="314"/>
         <source>Configure live persistence</source>
-        <translation>Configurer la persistance en direct</translation>
+        <translation>Configurer la persistance &quot;live&quot;</translation>
     </message>
     <message>
         <location filename="../mxremastercc.cpp" line="48"/>
@@ -107,7 +105,7 @@
     <message>
         <location filename="../mxremastercc.cpp" line="89"/>
         <source>This program provides access to different remaster and persistence tools in MX Linux</source>
-        <translation>Ce programme donne accès à différents outils de personalisation et persistance dans MX Linux</translation>
+        <translation>Ce programme donne accès à différents outils de remastérisation et de persistance de MX Linux</translation>
     </message>
     <message>
         <location filename="../mxremastercc.cpp" line="91"/>
