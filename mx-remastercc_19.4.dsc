fc4de8fc7d06b15214fdc98e260affcfd4018c40
Adrian <adrian@mxlinux.org>
2019-04-05 11:52:49 -0400

    update translations

---
+++ mx-remastercc_19.4.dsc
@@ -0,0 +1,32 @@
+-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA256
+
+Format: 3.0 (native)
+Source: mx-remastercc
+Binary: mx-remastercc
+Architecture: any
+Version: 19.4
+Maintainer: Adrian <adrian@mxlinux.org>
+Standards-Version: 3.9.8
+Vcs-Git: git://github.com/AdrianTM/mx-remastercc
+Build-Depends: qt5-default, qttools5-dev-tools, debhelper (>= 9)
+Package-List:
+ mx-remastercc deb admin optional arch=any
+Checksums-Sha1:
+ 14fff84c21caa6d7bf976209aa380b850abdc654 32944 mx-remastercc_19.4.tar.xz
+Checksums-Sha256:
+ f43730484bba8e9ce14867b4458b73d8bb0356f60ce7afa4b984f766ccde0347 32944 mx-remastercc_19.4.tar.xz
+Files:
+ a9b8705e2b8fbcb66a834276c8aff918 32944 mx-remastercc_19.4.tar.xz
+
+-----BEGIN PGP SIGNATURE-----
+
+iQFHBAEBCAAxFiEE8ndToY6S45N+YzXncJOMeAZ57pgFAlynebwTHGFkcmlhbkBt
+eGxpbnV4Lm9yZwAKCRBwk4x4BnnumKZJB/9+0U/+mF7WadkmHvS7b4Kbf/NL2X0T
+R7miavLSkabCR8+oZXV73QFgWIDDpueIufmF9LpD487X77DqbeCKCQw9pbI6XApi
+S+gxUenPpUB6LYfnlxM9uOB+gI127/0rk7AeSP4WVpSy9fKlQeZ0pC3enhVPcXLE
+iAG8yC/pFQzrgUNXK/2x/kJRTGRKTS2A8uoi7P3HEb1lJRzkLJkvbyDMx7Z82pdp
+iJqZratyzQLo2v7FEELcOVnQRlfLRh0R/Zzx5mByLML3f4SJ2haWN8qeLT2ksR6K
+7PkwgmoA9BFHhxvFzCMxBhIcfCHxCR3FKVEYtP3ofGutPhbIhh08trLm
+=Gz+i
+-----END PGP SIGNATURE-----
--- translations/mx-remastercc_pl.ts
+++ translations/mx-remastercc_pl.ts
@@ -1,134 +1,136 @@
-<?xml version="1.0" ?><!DOCTYPE TS><TS language="pl" version="2.1">
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="pl">
 <context>
     <name>mxremastercc</name>
     <message>
-        <location filename="mxremastercc.ui" line="14"/>
+        <location filename="../mxremastercc.ui" line="14"/>
         <source>MX Remastercc</source>
         <translation>MX Remaster</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="64"/>
+        <location filename="../mxremastercc.ui" line="64"/>
         <source>Persistence</source>
         <translation>Persistence</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="81"/>
+        <location filename="../mxremastercc.ui" line="81"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;This tool allows you to rework an ISO while running Live. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remaster&lt;/span&gt; allows you to change files, applications and settings with the goal of producing a new master copy for storage or distribution. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistence&lt;/span&gt; allows you to retain data, settings and installed programs when the machine gets switched off for personal use.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;To narzędzie pozwala na stworzenie nowej wersji ISO podczas korzystania z trybu Live. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remaster&lt;/span&gt; umożliwia zmianę plików, aplikacji i ustawień w celu utworzenia nowej kopii głównej do przechowywania lub dystrybucji. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistence&lt;/span&gt; pozwala zachować dane, ustawienia i zainstalowane programy do użytku osobistego, gdy urządzenie zostanie wyłączone.</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="133"/>
+        <location filename="../mxremastercc.ui" line="133"/>
         <source>Display help </source>
         <translation>Wyświetl pomoc</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="136"/>
+        <location filename="../mxremastercc.ui" line="136"/>
         <source>Help</source>
         <translation>Pomoc</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="143"/>
+        <location filename="../mxremastercc.ui" line="143"/>
         <source>Alt+H</source>
         <translation>Alt+H</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="175"/>
+        <location filename="../mxremastercc.ui" line="175"/>
         <source>About this application</source>
         <translation>O programie</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="178"/>
+        <location filename="../mxremastercc.ui" line="178"/>
         <source>About...</source>
         <translation>O...</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="185"/>
+        <location filename="../mxremastercc.ui" line="185"/>
         <source>Alt+B</source>
         <translation>Alt+B</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="217"/>
+        <location filename="../mxremastercc.ui" line="217"/>
         <source>Quit application</source>
         <translation>Zakończ aplikację</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="220"/>
+        <location filename="../mxremastercc.ui" line="220"/>
         <source>Close</source>
         <translation>Zamknij</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="227"/>
+        <location filename="../mxremastercc.ui" line="227"/>
         <source>Alt+N</source>
         <translation>Alt+N</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="264"/>
+        <location filename="../mxremastercc.ui" line="264"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Basic operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Działania podstawowe&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="274"/>
+        <location filename="../mxremastercc.ui" line="274"/>
         <source>Remaster</source>
         <translation>Remaster</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="300"/>
+        <location filename="../mxremastercc.ui" line="300"/>
         <source>Save root persistence</source>
         <translation>Zapisz root persistence</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="313"/>
+        <location filename="../mxremastercc.ui" line="313"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Optional operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Działania opcjonalne&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="mxremastercc.ui" line="323"/>
+        <location filename="../mxremastercc.ui" line="323"/>
         <source>Configure live persistence</source>
         <translation>Konfiguracja live persistence</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="51"/>
-        <location filename="mxremastercc.cpp" line="100"/>
+        <location filename="../mxremastercc.cpp" line="51"/>
+        <location filename="../mxremastercc.cpp" line="100"/>
         <source>MX Remaster Control Center</source>
         <translation>MX Remaster Centrum Kontroli</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="99"/>
+        <location filename="../mxremastercc.cpp" line="99"/>
         <source>About MX Remaster Control Center</source>
         <translation>O programie MX Remaster Centrum Kontroli</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="100"/>
+        <location filename="../mxremastercc.cpp" line="100"/>
         <source>Version: </source>
         <translation>Wersja:</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="101"/>
+        <location filename="../mxremastercc.cpp" line="101"/>
         <source>This program provides access to different remaster and persistence tools in MX Linux</source>
         <translation>Ten program zapewnia dostęp do różnych narzędzi remasterujących i utrwalających w systemie MX Linux</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="103"/>
+        <location filename="../mxremastercc.cpp" line="103"/>
         <source>Copyright (c) MX Linux</source>
         <translation>Prawa autorskie © MX Linux</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="104"/>
+        <location filename="../mxremastercc.cpp" line="104"/>
         <source>License</source>
         <translation>Licencja</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="105"/>
+        <location filename="../mxremastercc.cpp" line="105"/>
         <source>Changelog</source>
         <translation>Dziennik zmian</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="106"/>
+        <location filename="../mxremastercc.cpp" line="106"/>
         <source>Cancel</source>
         <translation>Anuluj</translation>
     </message>
     <message>
-        <location filename="mxremastercc.cpp" line="122"/>
+        <location filename="../mxremastercc.cpp" line="122"/>
         <source>&amp;Close</source>
         <translation>&amp;Zamknij</translation>
     </message>
