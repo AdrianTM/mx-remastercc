e8c8afe8bce606e1ebd311011ea750d8e82744b4
Dolphin Oracle <dolphinoracle@gmail.com>
2022-07-23 10:34:39 -0400

    add get_translations

---
+++ translations/mx-remastercc_fil_PH.ts
@@ -0,0 +1,136 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS version="2.1" language="fil_PH">
+<context>
+    <name>mxremastercc</name>
+    <message>
+        <location filename="mxremastercc.ui" line="14"/>
+        <source>MX Remastercc</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="64"/>
+        <source>Persistence</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="81"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p align=&quot;center&quot;&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;This tool allows you to rework an ISO while running Live. &lt;/span&gt;&lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Remaster&lt;/span&gt; allows you to change files, applications and settings with the goal of producing a new master copy for storage or distribution. &lt;/p&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Persistence&lt;/span&gt; allows you to retain data, settings and installed programs when the machine gets switched off for personal use.&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="133"/>
+        <source>Display help </source>
+        <translation>Maghanap ng tulong</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="136"/>
+        <source>Help</source>
+        <translation>Tulong</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="143"/>
+        <source>Alt+H</source>
+        <translation>Alt+H</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="175"/>
+        <source>About this application</source>
+        <translation>Patungkol sa application na ito</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="178"/>
+        <source>About...</source>
+        <translation>Patungkol sa...</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="185"/>
+        <source>Alt+B</source>
+        <translation>Alt+B</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="217"/>
+        <source>Quit application</source>
+        <translation>Iwan ang application</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="220"/>
+        <source>Close</source>
+        <translation>Isara</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="227"/>
+        <source>Alt+N</source>
+        <translation>Alt+N</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="264"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Basic operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="274"/>
+        <source>Remaster</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="300"/>
+        <source>Save root persistence</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="313"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline;&quot;&gt;Optional operations&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.ui" line="323"/>
+        <source>Configure live persistence</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="51"/>
+        <location filename="mxremastercc.cpp" line="100"/>
+        <source>MX Remaster Control Center</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="99"/>
+        <source>About MX Remaster Control Center</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="100"/>
+        <source>Version: </source>
+        <translation>Bersyon: </translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="101"/>
+        <source>This program provides access to different remaster and persistence tools in MX Linux</source>
+        <translation type="unfinished"/>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="103"/>
+        <source>Copyright (c) MX Linux</source>
+        <translation>Karapatan (c) MX Linux</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="104"/>
+        <source>License</source>
+        <translation>Lisensya</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="105"/>
+        <source>Changelog</source>
+        <translation>Ulat na mga pagbabago</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="106"/>
+        <source>Cancel</source>
+        <translation>I-cancel</translation>
+    </message>
+    <message>
+        <location filename="mxremastercc.cpp" line="122"/>
+        <source>&amp;Close</source>
+        <translation>&amp;Lumabas</translation>
+    </message>
+</context>
+</TS>
